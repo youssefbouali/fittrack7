2379f13fd4fe0befc795614b6823dffca69099df
Bouali <youssefbouali206@gmail.com>
2025-11-05 02:47:00 +0100

    m

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.elastic_beanstalk.id]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
